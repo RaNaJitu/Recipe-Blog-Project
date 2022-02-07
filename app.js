1ec4d9d3d401c1044e69dd4e94f5b170834351d4
RaNaJitu <jitendrashivmendir@gmail.com>
2022-02-07 17:42:40 +0530

    css file add

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ app.use(express.urlencoded({ extended: true}));
 
 app.use(express.static('public'));
 
-app.set('layout','./layouts/main');
-app.set('view engine', 'ejs')
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
 
-const routes = require('./server/routes/recipeRoutes.js');
-app.use('/',routes)
+const routes = require('./server/routes/recipeRoutes.js')
+app.use('/', routes);
 
-app.listen(port, ()=>{ console.log(`Listening to port ${port}`) })
+app.listen(port, ()=> console.log(`Listening to port ${port}`));
